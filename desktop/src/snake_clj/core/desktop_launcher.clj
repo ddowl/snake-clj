95b05fd48a38808f8b29aca83836ebe264022fca
Drew Dowling <drewdowling37@gmail.com>
2020-05-11 14:52:08 -0700

    setup play-clj project

---
+++ desktop/src/snake_clj/core/desktop_launcher.clj
@@ -0,0 +1,10 @@
+(ns snake-clj.core.desktop-launcher
+  (:require [snake-clj.core :refer :all])
+  (:import [com.badlogic.gdx.backends.lwjgl LwjglApplication]
+           [org.lwjgl.input Keyboard])
+  (:gen-class))
+
+(defn -main
+  []
+  (LwjglApplication. snake-clj-game "snake-clj" 800 600)
+  (Keyboard/enableRepeatEvents true))
