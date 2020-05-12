c19795563d631a194973eee3081010f7b5ea751a
Drew Dowling <drewdowling37@gmail.com>
2020-05-11 20:28:18 -0700

    create basic game loop

--- src/snake_clj/core.clj
+++ src/snake_clj/core.clj
@@ -1,6 +1,12 @@
 (ns snake-clj.core
   (:require [clojure.term.colors :as tc]))
 
+; =========== UPDATE ==========
+
+(defn update-game [state] state)
+
+; =========== VIEW ============
+
 (def block "██")
 
 (defn print-row [n]
@@ -10,10 +16,23 @@
 (defn print-grid [w h]
   (dorun (repeatedly h #(print-row w))))
 
+(defn clear [] (print (str (char 27) "[2J")))
+
+; ========= MAIN ==========
+
 (defn -main
   "Runs a game of Snake in the terminal"
   [& args]
   (let [grid-x 15
         grid-y 10]
-    (print-grid grid-x grid-y)))
-
+    (loop [state {:food [0 0]
+                  :snake {:body [[2 1] [2 2] [2 3]]
+                          :dir :up}
+                  :game-over? false}]
+      (if (:game-over? state)
+        (println "Game Over!")
+        (let [new-state (update-game state)]
+          (clear)
+          (print-grid grid-x grid-y)
+          (Thread/sleep 100)
+          (recur new-state))))))
