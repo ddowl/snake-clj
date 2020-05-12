d115f44766ac220a8ab0c4818ede52dadc461c37
Drew Dowling <drewdowling37@gmail.com>
2020-05-11 21:24:13 -0700

    view prints current game state

--- src/snake_clj/core.clj
+++ src/snake_clj/core.clj
@@ -9,12 +9,19 @@
 
 (def block "██")
 
-(defn print-row [n]
-  (dorun (repeatedly n #(print (tc/yellow block))))
-  (println))
-
-(defn print-grid [w h]
-  (dorun (repeatedly h #(print-row w))))
+(defn print-game [state]
+  (let [food (:food state)
+        snake (get-in state [:snake :body])
+        width (:width state)
+        height (:height state)]
+    (doseq [y (range height)]
+      (doseq [x (range width)]
+        (let [curr-coords [x y]]
+          (cond
+            (= curr-coords food) (print (tc/red block))
+            (some #(= curr-coords %) snake) (print (tc/green block))
+            :else (print (tc/yellow block)))))
+      (println))))
 
 (defn clear [] (print (str (char 27) "[2J")))
 
@@ -23,16 +30,16 @@
 (defn -main
   "Runs a game of Snake in the terminal"
   [& args]
-  (let [grid-x 15
-        grid-y 10]
-    (loop [state {:food [0 0]
-                  :snake {:body [[2 1] [2 2] [2 3]]
-                          :dir :up}
-                  :game-over? false}]
-      (if (:game-over? state)
-        (println "Game Over!")
-        (let [new-state (update-game state)]
-          (clear)
-          (print-grid grid-x grid-y)
-          (Thread/sleep 100)
-          (recur new-state))))))
+  (loop [state {:food [4 5]
+                :snake {:body [[2 1] [2 2] [2 3]]
+                        :dir :down}
+                :game-over? false
+                :width 15
+                :height 10}]
+    (if (:game-over? state)
+      (println "Game Over!")
+      (let [new-state (update-game state)]
+        (clear)
+        (print-game new-state)
+        (Thread/sleep 100)
+        (recur new-state)))))
